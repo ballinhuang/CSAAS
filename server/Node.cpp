8770d4a579b72ce64ce94cc2bd7ec448c77767c7
GC Lee <xyz797979@gmail.com>
2017-07-29 20:05:43 +0800

    FIFO

--- server/Node.cpp
+++ server/Node.cpp
@@ -4,6 +4,18 @@
 using namespace std;
 
 
+Node::Node(){
+    // null
+}
+
+Node::Node(string ip, string port, string name, int core){
+    setnodeip(ip);
+    setnodeport(port);
+    setnodename(name);
+    setCPUcore(core);
+    original_CPU_core = core;
+}
+
 string Node::getnodeip(){
     return nodeip;
 }
@@ -18,6 +30,10 @@ string Node::getnodename(){
 
 int Node::getnodeCPUcore(){
     return CPU_core;
+}
+
+int Node::getoriginalCPUcore(){
+    return original_CPU_core;
 }
 
 void Node::setnodeip(string ip){
