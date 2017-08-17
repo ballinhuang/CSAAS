df98f2efc19d71a6d7b771dd2649dda7b18aa038
ballinhuang <x3562151@gmail.com>
2017-08-17 18:26:31 +0800

    server -i -p -si -sp support

--- mom/main.cpp
+++ mom/main.cpp
@@ -12,66 +12,77 @@ JobQueue *JobQueue::jobquque = 0;
 
 int debug = 0;
 ofstream *debug_file;
-int main(int argc, char **argv){
-    
-    if(argc > 1){
+int main(int argc, char **argv)
+{
+
+    if (argc > 1)
+    {
         string arg;
         arg = std::string(argv[1]);
-        if(arg == "-fdebug"){
+        if (arg == "-fdebug")
+        {
             debug = 1;
             debug_file = new ofstream();
             debug_file->open("debug.txt");
         }
-        else if(arg == "-odebug"){
+        else if (arg == "-odebug")
+        {
             debug = 2;
         }
-        else{
-            cout << "MOM ---> main(): Error command ("<< arg << ") !" << endl;
+        else
+        {
+            cout << "MOM ---> main(): Error command (" << arg << ") !" << endl;
             exit(1);
         }
     }
 
-    string server_ip,server_port;
+    string server_ip, server_port;
     ifstream f("server.con");
-    if(f.is_open()){
+    if (f.is_open())
+    {
         f >> server_ip;
         f >> server_port;
     }
-    else{
+    else
+    {
         cout << "MOM ---> main(): Error! server.con not found." << endl;
         exit(1);
     }
     f.close();
-    string mom_ip,mom_port;
+    string mom_ip, mom_port;
     f.open("mom.con");
-    if(f.is_open()){
+    if (f.is_open())
+    {
         f >> mom_ip;
         f >> mom_port;
     }
-    else{
+    else
+    {
         cout << "MOM ---> main(): Error! mom.con not found." << endl;
         exit(1);
     }
     f.close();
 
-    pid_t pid= fork();
-	if(pid == -1) {
-    	cout << "mom server fork error !" << endl;
+    pid_t pid = fork();
+    if (pid == -1)
+    {
+        cout << "mom server fork error !" << endl;
         exit(EXIT_FAILURE);
     }
-    else if(pid != 0) {
-        if(debug > 0){
-            if(debug == 1)
+    else if (pid != 0)
+    {
+        if (debug > 0)
+        {
+            if (debug == 1)
                 *debug_file << "mom shut down parent !" << endl;
-            else if(debug == 2)
+            else if (debug == 2)
                 cout << "mom shut down parent !" << endl;
         }
-    	return 0;
+        return 0;
     }
     Mom mom;
     JobQueue::GetInstance()->attachmom(&mom);
-    mom.set_server_attr(server_ip,server_port);
-    mom.set_mom_attr(mom_ip,mom_port);
+    mom.set_server_attr(server_ip, server_port);
+    mom.set_mom_attr(mom_ip, mom_port);
     mom.run();
-
 }
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-cd server -odebug
-./server
+cd server 
+./server -odebug -i 127.0.0.1 -p 5001 -si 127.0.0.1 -sp 5002
 cd ..
 
 cd mom
@@ -9,3 +9,4 @@ cd ..
 cd scheduler
 ./scheduler -odebug -mode FIFO
 cd ..
+
